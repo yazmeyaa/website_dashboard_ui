973e888cf43cb2a2be226d1bcc01449f2f3587b5
= <=>
2024-03-30 12:52:27 +0300

    feat: update build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import dotenv from "dotenv";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const envFile =
+    mode === "development" ? ".env.development" : ".env.production";
+  dotenv.config({ path: envFile });
+  return {
+    plugins: [react()],
+    build: {
+      outDir: "build",
+      assetsDir: "assets",
+      emptyOutDir: true,
+    },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  };
+});

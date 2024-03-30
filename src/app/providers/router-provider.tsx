b1854bac0d81e73103481fe6014aa39e1bd6bd37
= <=>
2024-03-30 13:59:10 +0300

    Update build strategy

--- src/app/providers/router-provider.tsx
+++ src/app/providers/router-provider.tsx
@@ -2,5 +2,5 @@ import { FC, ReactNode } from "react";
 import { BrowserRouter } from "react-router-dom";
 
 export const RouterProvider: FC<{ children: ReactNode }> = ({ children }) => {
-  return <BrowserRouter>{children}</BrowserRouter>;
+  return <BrowserRouter basename="/admin">{children}</BrowserRouter>;
 };
